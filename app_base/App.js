91aa1c0900e5b868e24dc54515d8d7cf789e3cbe
Luis Hernandez <43506296+xWesko@users.noreply.github.com>
2021-09-13 11:07:38 -0700

    first commit

---
+++ app_base/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+      <Text style={{ fontSize: 20, color: '#777'  }}> Base App </Text>
+      <Text style={{ fontSize: 12, color: '#777'  }}> Luis Hernandez </Text>
+      
+    </SafeAreaView>
+  );
+
+};
+
+
+
+export default App;
---
+++ app_base/index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ * Escrito por Luis Hernandez
+ * luisestrada.ti@gmail.com
+ * 13 sep 2021
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
